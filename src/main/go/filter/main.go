6ae578b3156e7ca8c5cacf1a79b5ab6c828633d1
peploleum <peploleum@gmail.com>
2021-06-19 21:15:10 +0200

    initialized main entrypoint

---
+++ src/main/go/filter/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	argsWithProg := os.Args
+	log.Println("Executing filter")
+	log.Println(argsWithProg)
+	log.Println(strings.Repeat("▔", 65))
+	log.Println(strings.Repeat("▔", 65))
+	log.Println(strings.Repeat("▔", 65))
+	log.Println("reading from stdin:")
+
+}
